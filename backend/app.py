58d42222e8f2555d075899387c857e038f6392e3
nabil.tiaiba <nabilnaba99@gmail.com>
2024-04-06 18:19:20 +0100

    adding GET, INSERT, UPDATE record in the backend

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,45 @@
 from fastapi import FastAPI
+import sqlite3
+from pydantic import BaseModel
 
+class Comment(BaseModel):
+    comment_id: int
+    compaign_id : int
+    comment_description: str
+    sentiment : str
 app=FastAPI()
 
 @app.get("/")
 def get_data():
-    return{'message':"Welcome to Home page"}
+    connection = sqlite3.connect("../Comments.db")
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM Comments")
+    record = cursor.fetchone()
+    cursor.close()
+    return{'message':record}
 
 @app.post("/insert")
-def insert_record():
-    #insert a record into sqlite db with provided comment_id,campaign_id, description, sentiment
-    return{'message':"new record inserted to database"}
+def insert_record(comment:Comment):
+    connection = sqlite3.connect("../Comments.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT into  Comments (comment_id,campaign_id,description,sentiment) values(?,?,?,?)",( comment.comment_id,comment.compaign_id, comment.comment_description,comment.sentiment))
+    connection.commit()
+    return{'message':"Comment has been added to the database"}
 
-@app.delete("/delete")
-def delete_record():
-    # delete comment from db based on comment_id
+@app.delete("/delete/{comment_id}")
+def delete_record(comment_id:int):
+    connection = sqlite3.connect("../Comments.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM  Comments WHERE comment_id= ?",(comment_id,))
+    connection.commit()
     return{'message':"record deleted"}
 
 @app.patch("/update")
-def update_record():
+def update_record(comment:Comment):
+    connection = sqlite3.connect("../Comments.db")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE Comments SET campaign_id=?, description=?,sentiment=? WHERE comment_id=?",(comment.compaign_id, comment.comment_description,comment.sentiment, comment.comment_id))
+    connection.commit()
     # update a record based on provided comment_id,campaign_id, description, sentiment
     return{'message':"record updated successfuly"}
 
